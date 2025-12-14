d717bb88fbf9c4ee64b8e570f6d5cc6286756632
Iuliia Stetskaia <juliastetskaya@gmail.com>
2025-12-14 11:40:14 +0300

    Add public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,8 +11,10 @@ import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
 import { RefreshDto } from './dto/refresh.dto';
+import { Public } from './public.decorator';
 
 @Controller('auth')
+@Public()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
